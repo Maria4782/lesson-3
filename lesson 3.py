28abd7ce2eaed896e7ec47f6220c1b106124ed04
Maria4782 <mashasalt2000@icloud.com>
2024-09-14 18:15:22 +0500

    Add files via upload

--- lesson 3.py
+++ lesson 3.py
@@ -1,6 +1,6 @@
 course='Python'
 home_works=12
 time=1.5
-print(1.5/12)
+print(time/home_works)
 timeForOneTask=0.125
 print('Курс',course,'Количество заданий',home_works, 'Затраченное время', time, 'Время на одно задание',timeForOneTask)
